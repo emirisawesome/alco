c86be12080b356ead78d25e5842fa47232198cf4
emirisawesome <emirisaev2000@gmail.com>
2022-01-13 23:08:11 +0600

    mvp

---
+++ js/script.js
@@ -0,0 +1,134 @@
+
+'use strict'
+
+const API = 'https://www.thecocktaildb.com/api/json/v1/1/'
+const SEARCH = 'search.php?s='
+const GET_ALL_COCKTAILS = `${API}filter.php?c=Cocktail`
+const FILTER_BY_ALCOHOLIC = 'filter.php?a='
+const LOOKUP_COCKTAIL_DETAIL = 'lookup.php?i='
+
+
+
+const body = document.body
+
+const searchInput = document.querySelector('.searchInput')
+const alcoholicFilter = document.querySelector('.alcoholicFilter')
+const output = document.querySelector('.output')
+
+
+const displayCocktails = async (data) => {
+    output.innerHTML = ''
+    console.log(data);
+    data.forEach(element => {
+        let card = document.createElement('article')
+        card.className = 'card'
+        card.addEventListener('click', () => {
+            getLookupCocktail(element)
+        })
+        output.append(card)
+
+        let cardImg = document.createElement('img')
+        cardImg.className = 'cardImg'
+        cardImg.src = element.strDrinkThumb
+        card.append(cardImg)
+
+        let cardTitle = document.createElement('h2')
+        cardTitle.className = 'cardTitle'
+        cardTitle.innerHTML = element.strDrink
+        card.append(cardTitle)
+
+
+    });
+
+
+}
+
+
+const displayLookup = async (data) => {
+    output.innerHTML = ''
+
+    let card = document.createElement('article')
+    card.className = 'card'
+    output.append(card)
+
+    let cardImg = document.createElement('img')
+    cardImg.className = 'cardImg'
+    cardImg.src = data.strDrinkThumb
+    card.append(cardImg)
+
+    let cardTitle = document.createElement('h2')
+    cardTitle.className = 'cardTitle'
+    cardTitle.innerHTML = data.strDrink
+    card.append(cardTitle)
+
+    let AlcoholicStatus
+    (data.strAlcoholic == 'Alcoholic') ? AlcoholicStatus = 'Алкогольный' : AlcoholicStatus = 'Без алкогольный'
+
+    let cocktailtype = document.createElement('h4')
+    cocktailtype.className = 'cocktailtype'
+    cocktailtype.innerHTML = `Тип: ${AlcoholicStatus}`
+    card.append(cocktailtype)
+
+    let cocktailGlass = document.createElement('h4')
+    cocktailGlass.className = 'cocktailGlass'
+    cocktailGlass.innerHTML = `Посуда: ${data.strGlass}`
+    card.append(cocktailGlass)
+
+    let cocktailInstructions = document.createElement('div')
+    cocktailInstructions.className = 'cocktailInstructions'
+    cocktailInstructions.innerHTML = `<h4>Инструкция:</h4> <p class='cocktailInstructionsPor'>${data.strInstructions}</p>`
+    card.append(cocktailInstructions)
+
+}
+
+const getAllCocktails = async () => {
+    const request = await fetch(GET_ALL_COCKTAILS)
+    const response = await request.json()
+
+    displayCocktails(response.drinks)
+}
+
+
+
+
+const getCocktailByName = async () => {
+    console.log('getCocktailByName');
+
+    const request = await fetch(`${API}${SEARCH}${searchInput.value}`)
+    const response = await request.json()
+
+    displayCocktails(response.drinks)
+
+}
+
+
+const getAlcoholicFilter = async (type) => {
+    const request = await fetch(`${API}${FILTER_BY_ALCOHOLIC}${type}`)
+    const response = await request.json()
+    displayCocktails(response.drinks)
+
+}
+
+const getLookupCocktail = async (id) => {
+    const request = await fetch(`${API}${LOOKUP_COCKTAIL_DETAIL}${id.idDrink}`)
+    const response = await request.json()
+
+    console.log(response.drinks[0]);
+    displayLookup(response.drinks[0])
+}
+
+
+searchInput.addEventListener('keypress', (e) => {
+
+    setTimeout(() => {
+        if (e.target.value.length >= 2) {
+            getCocktailByName()
+        }
+    }, 100)
+
+})
+
+alcoholicFilter.addEventListener('change', (e) => {
+    getAlcoholicFilter(e.target.value)
+})
+getAllCocktails()
